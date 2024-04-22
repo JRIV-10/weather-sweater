8a9952572cc90006818ce6a007e82a7e493a158b
Joey Reyes <joeyreyes105@gmail.com>
2024-04-22 06:59:37 -0700

    still need testing for poro, facade and service

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,11 @@
+class ForecastFacade
+
+  def self.forecast_location(location)
+    ForecastService.city_coordinates(location)[:results][0][:locations][0][:latLng]
+  end
+
+  def self.weather_forecast(coords)
+    data = ForecastService.forecast_data(coords)
+    Forecast.new(data)
+  end
+end
