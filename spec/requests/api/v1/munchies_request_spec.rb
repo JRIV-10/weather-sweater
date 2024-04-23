e74b55605d4b4cc9e80479224004b8d0dc5bc6e6
Joey Reyes <joeyreyes105@gmail.com>
2024-04-23 10:09:35 -0700

    finish request spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -41,7 +41,19 @@ RSpec.describe "Yelp API Request", type: :request do
     end
 
     it "returns a resturant based off the params" do
-        
+      expect(response).to have_http_status(200)
+
+        data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(data[:type]).to eq("munchie")
+        expect(data[:id]).to eq(nil)
+        expect(data[:attributes][:destination_city]).to eq("Denver, CO")
+        expect(data[:attributes][:forecast][:summary]).to eq("Partly cloudy")
+        expect(data[:attributes][:forecast][:temperature]).to eq(48.2)
+        expect(data[:attributes][:restaurant][:name]).to eq("Vinny and Marie’s Italian Street Food")
+        expect(data[:attributes][:restaurant][:address]).to eq("1477 Monroe St, Denver, 80206, US")
+        expect(data[:attributes][:restaurant][:rating]).to eq(5)
+        expect(data[:attributes][:restaurant][:reviews]).to eq(17)
     end
   end 
 end
