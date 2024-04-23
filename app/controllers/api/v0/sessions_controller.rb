f43a47286d73d0898c704e21ee1fc1a7649e0f9a
Joey Reyes <joeyreyes105@gmail.com>
2024-04-23 13:35:04 -0700

    finish controller

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -4,7 +4,13 @@ class Api::V0::SessionsController < ApplicationController
     if valid_user?(user)
       render json: UserSerializer.serialize(user), status: 200
     else 
-      render 
+      render json: ErrorSerializer.serialize(ErrorMessage.new("Error: Email/Password incorrect", 401)), status: 401
     end
+  end
+
+  private
+
+  def valid_user?(user) 
+    user && user.authenticate(params[:password])
   end
 end
